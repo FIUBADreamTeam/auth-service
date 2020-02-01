dfdb883a6a6c378a1a571a789095084b48e063d3
jorgejcabrera <cabrerajjorge@gmail.com>
2020-02-01 16:32:42 -0300

    initial commit

---
+++ src/test/kotlin/com/fdt/authservice/AuthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fdt.authservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
